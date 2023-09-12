d257a1b967eedd218c6e657bcaa5826ef0bceca8
samuel-soto2 <1461888@senati.pe>
2023-09-12 15:49:55 -0500

    aqui tenemos ya una imagen, tambien tenemos para que pueda escribir su usuario y su contraseña

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
             cerrar = new Button();
+            pictureBox1 = new PictureBox();
+            user = new TextBox();
+            pass = new TextBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -41,7 +45,7 @@
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(638, 37);
+            panel1.Size = new Size(489, 37);
             panel1.TabIndex = 0;
             // 
             // cerrar
@@ -51,29 +55,67 @@
             cerrar.FlatAppearance.MouseDownBackColor = Color.White;
             cerrar.FlatAppearance.MouseOverBackColor = Color.White;
             cerrar.Image = (Image)resources.GetObject("cerrar.Image");
-            cerrar.Location = new Point(586, 3);
+            cerrar.Location = new Point(436, 3);
             cerrar.Name = "cerrar";
             cerrar.Size = new Size(40, 31);
             cerrar.TabIndex = 0;
             cerrar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(179, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(179, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // user
+            // 
+            user.BorderStyle = BorderStyle.None;
+            user.Font = new Font("Yu Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            user.Location = new Point(109, 198);
+            user.Name = "user";
+            user.Size = new Size(304, 21);
+            user.TabIndex = 2;
+            user.Text = "ingrese su usuario";
+            // 
+            // pass
+            // 
+            pass.BorderStyle = BorderStyle.None;
+            pass.Font = new Font("Yu Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            pass.Location = new Point(97, 277);
+            pass.Name = "pass";
+            pass.Size = new Size(304, 21);
+            pass.TabIndex = 3;
+            pass.Text = "ingrese su contraseña";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(638, 479);
+            ClientSize = new Size(489, 617);
+            Controls.Add(pass);
+            Controls.Add(user);
+            Controls.Add(pictureBox1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
         private Button cerrar;
+        private PictureBox pictureBox1;
+        private TextBox user;
+        private TextBox pass;
     }
 }
